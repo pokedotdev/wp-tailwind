bf95d461444704d2dd045a8b94b77ab4371bdf7f
Arturo Enriquez <arturoccompany@gmail.com>
2021-02-09 15:34:30 -0700

    add theme files

---
+++ footer.php
@@ -0,0 +1,10 @@
+<footer>
+  <h1 class="text-6xl">footer</h1>
+</footer>
+
+</body>
+<script id="__bs_script__">
+  document.write("<script async src='http://HOST:3000/browser-sync/browser-sync-client.js?v=2.26.13'><\/script>".replace("HOST", location.hostname));
+</script>
+
+</html>
---
+++ index.php
@@ -0,0 +1,7 @@
+<?php get_header(); ?>
+
+<main>
+  <h1 class="text-8xl m-6 block">INDEX</h1>
+</main>
+
+<?php get_footer(); ?>
